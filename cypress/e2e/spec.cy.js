94b6143cdfc1bb03c6802c678fe3b75347dd2c99
DamonHolden <damonh003@gmail.com>
2022-07-27 08:31:45 +0100

    feat: add more tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,34 @@
-describe('website', () => {
-  it('should load', () => {
+describe('landing page', () => {
+  it('should load when visited', () => {
     cy.visit('http://localhost:3000/');
+  });
+
+  it('should display h1', () => {
+    cy.contains('Cypress Deep Dive');
+  });
+});
+
+describe('hello-message button', () => {
+  it('should be visible', () => {
+    cy.contains('show hello message');
+  });
+
+  it('should be clickable', () => {
+    cy.contains('show hello message').click();
+  });
+
+  it('should then display the hello message', () => {
+    cy.contains('hello user');
+  });
+});
+
+describe('alert button', () => {
+  it('should be visible', () => {
+    cy.contains('alert user');
+  });
+
+  it('should be clickable', () => {
+    cy.contains('alert user').click();
   });
 });
 
@@ -8,7 +36,8 @@ describe('true', () => {
   it('should equal true', () => {
     expect(true).to.equal(true);
   });
-  it('should equal false', () => {
-    expect(true).to.equal(false);
-  });
+
+  // it('should equal false', () => {
+  //   expect(true).to.equal(false);
+  // });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import React, { useState } from 'react';
+
 function App() {
+  const [helloMessage, setHelloMessage] = useState(false);
   return (
     <div className='App'>
       <h1>Cypress Deep Dive</h1>
+      <button
+        onClick={() => {
+          setHelloMessage(!helloMessage);
+        }}
+      >
+        show hello message
+      </button>
+      <button
+        onClick={() => {
+          alert('button clicked');
+        }}
+      >
+        alert user
+      </button>
+      {helloMessage && <h2>hello user</h2>}
     </div>
   );
 }
